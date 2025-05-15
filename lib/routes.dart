05de145f5c7a353d84ffa1dcb2de12c24b979f88
VictorBren0 <victor.bsr2000@gmail.com>
2025-05-14 22:57:59 -0300

    fix: adicionado comentários para ignorar nomes de constantes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,8 @@
 class Routes {
+  // ignore: constant_identifier_names
   static const String HOME = '/';
+  // ignore: constant_identifier_names
   static const String FORM = '/form';
+  // ignore: constant_identifier_names
   static const String DETAILS = '/details';
 }
